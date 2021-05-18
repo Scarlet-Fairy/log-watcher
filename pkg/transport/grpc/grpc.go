e65c077c138ed08871faff1d992b91e8d5bfe1d8
Michele Della Mea <michele.dellamea.arcanediver@gmail.com>
2021-05-18 18:36:09 +0200

    grpc transport

--- pkg/transport/grpc/grpc.go
+++ pkg/transport/grpc/grpc.go
@@ -1,1 +1,39 @@
 package grpc
+
+import (
+	"context"
+	"github.com/Scarlet-Fairy/log-watcher/pb"
+	"github.com/Scarlet-Fairy/log-watcher/pkg/endpoint"
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/transport"
+	grpctransport "github.com/go-kit/kit/transport/grpc"
+)
+
+type grpcServer struct {
+	pb.UnimplementedLogWatcherServer
+	getLogs grpctransport.Handler
+}
+
+func NewGRPCServer(endpoints endpoint.LogWatcherEndoint, logger log.Logger) pb.LogWatcherServer {
+	options := []grpctransport.ServerOption{
+		grpctransport.ServerErrorHandler(transport.NewLogErrorHandler(logger)),
+	}
+
+	return &grpcServer{
+		getLogs: grpctransport.NewServer(
+			endpoints.GetLogsEndpoint,
+			decodeGetLogsRequest,
+			encodeDeployResponse,
+			options...,
+		),
+	}
+}
+
+func (g grpcServer) GetLogs(ctx context.Context, request *pb.GetLogsRequest) (*pb.GetLogsResponse, error) {
+	_, resp, err := g.getLogs.ServeGRPC(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.(*pb.GetLogsResponse), nil
+}
--- pkg/transport/grpc/request_response.go
+++ pkg/transport/grpc/request_response.go
@@ -1,1 +1,32 @@
 package grpc
+
+import (
+	"context"
+	"github.com/Scarlet-Fairy/log-watcher/pb"
+	"github.com/Scarlet-Fairy/log-watcher/pkg/endpoint"
+)
+
+func decodeGetLogsRequest(_ context.Context, grpcReq interface{}) (interface{}, error) {
+	req := grpcReq.(*pb.GetLogsRequest)
+
+	return &endpoint.GetLogsRequest{
+		JobId:  req.DeployId,
+		Offset: req.Offset,
+		Size:   req.Size,
+	}, nil
+}
+
+func encodeDeployResponse(_ context.Context, resp interface{}) (interface{}, error) {
+	res := resp.(*endpoint.GetLogsResponse)
+
+	var logs []*pb.Log
+	for _, log := range res.Logs {
+		logs = append(logs, &pb.Log{
+			Timestamp: log.Timestamp.String(),
+			Body:      log.Message,
+		})
+	}
+	return &pb.GetLogsResponse{
+		Logs: logs,
+	}, nil
+}
