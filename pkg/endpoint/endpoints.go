1017361e40d13c86e2994c8f0701610039432731
Michele Della Mea <michele.dellamea.arcanediver@gmail.com>
2021-05-18 18:35:30 +0200

    endpoint

--- pkg/endpoint/endpoints.go
+++ pkg/endpoint/endpoints.go
@@ -1,1 +1,62 @@
 package endpoint
+
+import (
+	"context"
+	"github.com/Scarlet-Fairy/log-watcher/pkg/service"
+	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/kit/log"
+)
+
+type LogWatcherEndoint struct {
+	GetLogsEndpoint endpoint.Endpoint
+}
+
+func NewEndpoint(s service.Service, logger log.Logger) LogWatcherEndoint {
+	var getLogsEndpoint endpoint.Endpoint
+	{
+		getLogsEndpoint = makeGetLogsEndpoint(s)
+		getLogsEndpoint = LoggingMiddleware(log.With(logger, "method", "GetLogs"))(getLogsEndpoint)
+		getLogsEndpoint = UnwrapErrorMiddleware()(getLogsEndpoint)
+	}
+
+	return LogWatcherEndoint{
+		GetLogsEndpoint: getLogsEndpoint,
+	}
+}
+
+var (
+	_ endpoint.Failer = GetLogsResponse{}
+)
+
+type GetLogsRequest struct {
+	JobId  string
+	Offset uint32
+	Size   uint32
+}
+
+type GetLogsResponse struct {
+	Logs []service.Log
+	Err  error `json:"-"`
+}
+
+func (r GetLogsResponse) Failed() error {
+	return r.Err
+}
+
+func makeGetLogsEndpoint(s service.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(*GetLogsRequest)
+
+		logs, err := s.QueryWorkload(
+			ctx,
+			req.JobId,
+			req.Offset,
+			req.Size,
+		)
+
+		return &GetLogsResponse{
+			Logs: logs,
+			Err:  err,
+		}, nil
+	}
+}
